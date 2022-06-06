351a2f9a5a7f9b329bf57e2c040f504bdc40d9a5
Evgenii Liventsev <liventsev@gmail.com>
2022-06-06 18:16:56 +0300

    more thoughts

--- design/fusion.tcl
+++ design/fusion.tcl
@@ -9,17 +9,50 @@
 
 [flow::check]
 
-[flow::start_script]
-set script_path $flow::config::script_dir/fusion_clock.tcl
+# this is like yaml merging
+[flow::script::start $flow::config::script_dir/fusion_clock.tcl]
 set flow::fusion::load_db::database /path/to/placed/database
-[flow::paste_step fusion.load_db]
-[flow::paste_step fusion.clock]
-[flow::paste_step fusion.save_db]
-[flow::write_script]
+[flow::script::paste_step fusion.init]
+[flow::script::paste_step fusion.load_db]
+[flow::script::add_tcl "
+  some\
+  tcl\
+  goes\
+  here\
+"]
+[flow::script::paste_step fusion.clock_design]
+[flow::script::paste_step fusion.save_db]
+[flow::script::write]
 
-foreach corner $flow::general::views {
-  [flow::start_script]
-  [flow::paste_step prime_time.setup]
-  [flow::paste_step prime_time.run_sta]
-  [flow::write_script]
+foreach view $flow::general::views {
+  [flow::script::start $flow::config::script_dir/${view}_sta.tcl]]
+  [flow::script::paste_step prime_time.init]
+  [flow::script::paste_step prime_time.run_sta]
+  [flow::script::write]
 }
+
+#---
+#flow:
+#  task: floorplan
+#    depends: none 
+#    tool: /path/to/fc
+#    script: /path/to/script/fp.tcl
+#    run_cmd: "$tool -script $script"
+#    resource_cmd: "qsub ..."
+#
+#  task: place
+#    depends: floorplan
+#    tool: /path/to/fc
+#    script: /path/to/script/place.tcl
+#    run_cmd: "$tool -script $script"
+#    resource_cmd: "qsub ..."
+#
+#  task: qrc
+#    depends: route
+#    tool: /path/to/qrc
+#    script: 
+#
+#  task: sta
+#    depends: [qrc, write_def]
+#    tool: /path/to/primetime
+#    script:
